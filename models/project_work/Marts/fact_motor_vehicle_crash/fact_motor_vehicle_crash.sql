e2eb3efcd99ff3d3a005874ba481adfa52b26a4d
Rojina Adhikari <baruch.rojina@gmail.com>
2026-04-24 23:39:06 +0000

    created a sql skeleton for the fact_motor_vehicle_crash

---
+++ models/project_work/Marts/fact_motor_vehicle_crash/fact_motor_vehicle_crash.sql
@@ -0,0 +1,33 @@
+WITH source AS (
+    SELECT *
+    FROM {{ ref('stg_motor_vehicle_crashes') }}
+),
+
+final AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['collision_id']) }} AS crash_skey,
+
+        collision_id,
+
+        -- TODO: add dimension foreign keys when teammates finish dimensions
+        NULL AS date_skey,
+        NULL AS location_skey,
+        NULL AS time_skey,
+
+        latitude,
+        longitude,
+
+        number_of_persons_injured AS num_persons_injured,
+        number_of_persons_killed AS num_persons_killed,
+        number_of_pedestrians_injured AS num_pedestrians_injured,
+        number_of_pedestrians_killed AS num_pedestrians_killed,
+        number_of_cyclist_injured AS num_cyclists_injured,
+        number_of_cyclist_killed AS num_cyclists_killed,
+        number_of_motorist_injured AS num_motorists_injured,
+        number_of_motorist_killed AS num_motorists_killed
+
+    FROM source
+)
+
+SELECT *
+FROM final
