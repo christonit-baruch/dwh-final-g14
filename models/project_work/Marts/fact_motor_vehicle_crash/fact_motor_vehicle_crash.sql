a0547eb6c79fd780b70011b07c369890fe030d72
Rojina Adhikari <baruch.rojina@gmail.com>
2026-04-25 19:42:09 +0000

    removed connection with dim_date for the fact_motor_vehicle_crash due to error

--- models/project_work/Marts/fact_motor_vehicle_crash/fact_motor_vehicle_crash.sql
+++ models/project_work/Marts/fact_motor_vehicle_crash/fact_motor_vehicle_crash.sql
@@ -2,39 +2,33 @@ WITH source AS (
     SELECT *
     FROM {{ ref('stg_motor_vehicle_collisions') }}
 ),
-dim_date AS (
-    SELECT *
-    FROM {{ ref('dim_date') }}
-),
 
 final AS (
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['collision_id']) }} AS crash_skey,
+        {{ dbt_utils.generate_surrogate_key(['collision_id']) }} AS crash_key,
 
         collision_id,
-        dim_date.date_key,
 
-        -- TODO: add dimension foreign keys when teammates finish dimensions
-    
-        NULL AS location_skey,
-        NULL AS time_skey,
+        -- temporary NULL keys until dimensions are successfully built
+        NULL AS date_key,
+        NULL AS time_key,
+        NULL AS location_key,
 
-        source.latitude,
-        source.longitude,
+        borough,
+        zip_code,
+        latitude,
+        longitude,
 
-        source.number_of_persons_injured,
-        source.number_of_persons_killed,
-        source.number_of_pedestrians_injured,
-        source.number_of_pedestrians_killed,
-        source.number_of_cyclist_injured,
-        source.number_of_cyclist_killed,
-        source.number_of_motorist_injured,
-        source.number_of_motorist_killed
+        number_of_persons_injured AS num_persons_injured,
+        number_of_persons_killed AS num_persons_killed,
+        number_of_pedestrians_injured AS num_pedestrians_injured,
+        number_of_pedestrians_killed AS num_pedestrians_killed,
+        number_of_cyclist_injured AS num_cyclists_injured,
+        number_of_cyclist_killed AS num_cyclists_killed,
+        number_of_motorist_injured AS num_motorists_injured,
+        number_of_motorist_killed AS num_motorists_killed
 
     FROM source
-
-    LEFT JOIN dim_date
-        ON CAST(source.crash_date AS DATE) = dim_date.full_date
 )
 
 SELECT *
